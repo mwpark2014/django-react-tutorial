ddc7f26f124bed3e946701a231fab8461b577f1c
Mason <park.mason.w@gmail.com>
2018-06-14 06:44:25 -0700

    Fill out views.py with generic ListCreateAPIView

--- project/leads/views.py
+++ project/leads/views.py
@@ -1,3 +1,9 @@
+from leads.models import Lead
+from leads.serializers import LeadSerializer
+from rest_framework import generics
 from django.shortcuts import render
 
 # Create your views here.
+class LeadListCreate(generics.ListCreateAPIView):
+  queryset = Lead.objects.all()
+  serializer_class = LeadSerializer
